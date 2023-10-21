4175b9c2338cdc69da063e1ea661d5cc9023d9c2
Chinraj K <chinraj.innov.con@phonepe.com>
2023-10-21 22:06:54 +0530

    Initial commit

---
+++ src/main/java/com/example/questionbankmanagementsystem/QuestionBankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionbankmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBankManagementSystemApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(QuestionBankManagementSystemApplication.class,args);
+    }
+
+}
---
+++ src/test/java/com/example/questionbankmanagementsystem/QuestionBankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionbankmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBankManagementSystemApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
